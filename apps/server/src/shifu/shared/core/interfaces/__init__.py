3b2c16fc94848b6ddc1bef18d3c3e33cdb8e94b2
JohnPetros <joaopcarvalho.cds@gmail.com>
2026-09-21 10:05:34 -0300

    fix(server): SHIFU-62 repair rate-limit merge integration

--- apps/server/src/shifu/shared/core/interfaces/__init__.py
+++ apps/server/src/shifu/shared/core/interfaces/__init__.py
@@ -1,12 +1,5 @@
 from .authentication_provider import AuthenticationProvider as AuthenticationProvider
-from .clock_provider import ClockProvider as ClockProvider
-from .events_repository import EventsRepository as EventsRepository
-from .events_repository_listener import (
-    EventsRepositoryListener as EventsRepositoryListener,
-)
-from .id_provider import IdentifierProvider as IdentifierProvider
-from .outbox_event import OutboxEvent as OutboxEvent
-from .authentication_provider import AuthenticationProvider as AuthenticationProvider
+from .cache_provider import CacheProvider as CacheProvider
 from .clock_provider import ClockProvider as ClockProvider
 from .events_repository import EventsRepository as EventsRepository
 from .events_repository_listener import (
